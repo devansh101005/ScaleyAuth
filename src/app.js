0fa138f5b3c6d91f007043d1e24936e49737e3ec
Devansh Pandey <devanshpandeyji4321@gmail.com>
2026-01-13 02:16:42 +0530

    projectstructure set

---
+++ src/app.js
@@ -0,0 +1,11 @@
+import express from "express"
+
+const app=express()
+const PORT = 3000
+
+app.use(express.json())
+
+
+app.listen(PORT, () => {
+    console.log(`Example app listening on port ${PORT}`)
+})
